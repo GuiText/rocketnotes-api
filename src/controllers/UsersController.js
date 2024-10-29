8e78d851f986230f4a009c9d821a2a8546726fe1
guilherme <guilhermesilva0281@gmail.com>
2024-10-29 06:59:04 -0300

    user creation function with email verification provided

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,14 +1,17 @@
 const AppError = require("../utils/AppError")
+const sqliteConnection = require("../database/sqlite")
 
 class UsersController {
-  create (request, response) {
+  async create (request, response) {
     const { name, email, password } = request.body
 
-    if(!name) {
-      throw new AppError("Nome é obrigatório")
-    }
+    const database = await sqliteConnection()
+    const checkUsersExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    response.status(201).json({ name, email, password })
+    if(checkUsersExists) {
+      throw new AppError("Este e-mail já está em uso")
+    }
+    return response.status(201).json()
   }
 }
 
